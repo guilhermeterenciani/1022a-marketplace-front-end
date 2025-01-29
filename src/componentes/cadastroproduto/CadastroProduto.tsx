a477c1d2bd362ee85df37bbc29f1a3669afeb7cb
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-01-29 10:49:55 -0400

    Update backend link

--- src/componentes/cadastroproduto/CadastroProduto.tsx
+++ src/componentes/cadastroproduto/CadastroProduto.tsx
@@ -10,7 +10,7 @@ function CadastroProduto(){
     async function handleForm(event:FormEvent){
         event.preventDefault()
         try{
-            const resposta = await fetch("http://localhost:8000/produtos",{
+            const resposta = await fetch("https://one022a-marketplace-e90o.onrender.com/produtos",{
                 method:"POST",
                 headers:{
                     "Content-Type":"application/json"
